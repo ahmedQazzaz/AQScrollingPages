c175fa123adbb70685013513dd6142b55603de57
Ahmed Qazzaz <ahmedqazzaz@iahmed.local>
2018-10-12 22:40:16 +0300

    Initial Commit

---
+++ ScrollingTaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingTaps
+//
+//  Created by Ahmed Qazzaz on 10/12/18.
+//  Copyright © 2018 Ahmed Qazzaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
